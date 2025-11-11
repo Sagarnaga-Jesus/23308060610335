cde16c110859cde3f40187b37fc525f2398a5b8b
Sagarnaga-Jesus <23308060610335@cetis61.edu.mx>
2025-11-10 23:59:36 -0700

    m,edioi

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ from flask import Flask, render_template,request,redirect
 
 app = Flask(__name__)
 
-alimentos=[]
+alimentos=[
+    {
+        "nombre": "Manzana",
+        "grasas": "0.2",
+        "proteinas": "0.3",
+        "carbohidratos": "14",
+        "agua": "0.85"
+    }
+]
 
 
 @app.route("/")
@@ -12,31 +20,36 @@ def formulario():
 @app.route("/resultado",methods=["POST","GET"])
 def resultado():
     if  request.method == ["POST"]:
-        nombre = request.form["nombre"]
+        nombre = request.form["alimento"]
         grasas = request.form["grasas"]
         prote = request.form["proteinas"]
         carbo = request.form["carbohidratos"]
+        agua = request.form["agua"]
         
         grasa=0
         proteinas=0
         carbohidratos=0
         
+        grasa = 9*float(grasas)
+        proteinas = 4*float(prote)
+        carbohidratos = 4*float(carbo)
         
+        
+        grasat = str(grasa)
+        proteinast = str(proteinas)
+        carbohidratost = str(carbohidratos)
         alimentos.append({
             "nombre": nombre ,
             "grasas": grasas ,
             "proteinas": prote ,
             "carbohidratos":carbo ,
-            
+            "grasasc":grasat,
+            "proteinasc":proteinast,
+            "carbohidratosc":carbohidratost
         })
         
-        grasa = 8*float(grasas)
-        proteinas = 4*float(prote)
-        carbohidratos = 4*float(carbo)
         
-        
-        
-    return render_template("resultado.html", nombre=nombre, grasa=grasa, proteinas=proteinas, carbohidratos=carbohidratos)
+    return render_template("resultado.html", alimentos=alimentos,)
 
 
 if __name__ == '__main__':
